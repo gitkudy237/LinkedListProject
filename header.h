2e46db58598c5291e7577c8bf65f9eb3da7844d2
kudy <bayardkudy1@gmail.com>
2024-08-16 16:32:19 +0100

    Declared and defined functions

--- header.h
+++ header.h
@@ -1,1 +1,12 @@
 
+// Beginning of Kudy's changes
+void display();
+int max();
+void add_student(int student);
+
+void display() {
+    printf("Displaying student info.");
+    printf("End of display");
+}
+
+// End of Kudy's changes
