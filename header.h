8bd948d1e3c1e76a220e0985b7a3320eab8dbfe9
ETUGE DERICK <dolass2017@gmail.com>
2024-08-16 19:50:31 +0100

    Update header.h code structure

--- header.h
+++ header.h
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+struct student {
+	int id;
+	int age;
+	char name[15];
+	int deleted;
+	struct student *next;
+};
+
+typedef struct student STUDENT;
+
+
+void display(STUDENT *s){
+	printf("\n*******************************\n");
+	printf("\nId: %d", s->id);
+	printf("\nAge: %d", s->age);
+	printf("\nDeleted: %d", s->deleted);
+	printf("\nName: %s", s->name);
+}
+
+void sortAsc(STUDENT *head){
+	
+}
+void initilize(STUDENT *head){
+	
+}
